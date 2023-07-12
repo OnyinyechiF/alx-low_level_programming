c519b2382c0d3fe1e557e7109eabf5b7d27babb4
OnyinyechiF <faithisaac684@gmail.com>
2023-07-12 05:50:53 -0700

    libmy.a, main.h

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -7,19 +7,23 @@
 * @src: copy from
 * Return: string
 */
-char *_strcpy(char *dest, char *src)
-{
-int l = 0;
-int x = 0;
 
-while (*(src + l) != '\0')
+char *_strcpy(char *dest, char *src)
+
 {
-l++;
-}
-for ( ; x < l ; x++)
-{
-dest[x] = src[x];
-}
-dest[l] = '\0';
-return (dest);
+
+	int l = 0;
+
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
